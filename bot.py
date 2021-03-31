b4f9d018e445dc356f77174c1b0551e1018afb7b
edwardzhuang2001 <45517287+edwardzhuang2001@users.noreply.github.com>
2021-03-31 18:35:08 -0400

    Second commit

--- bot.py
+++ bot.py
@@ -1,1 +1,11 @@
 import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix='.')
+token = ''
+
+@client.event
+async def on_ready():
+    print("Online.")
+
+client.run(token)
