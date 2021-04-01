9d4d96475c704d9d787c0c320d31995ed5fd6ae5
edwardzhuang2001 <45517287+edwardzhuang2001@users.noreply.github.com>
2021-03-31 23:24:44 -0400

    Added op.gg

--- bot.py
+++ bot.py
@@ -2,10 +2,15 @@ import discord
 from discord.ext import commands
 
 client = commands.Bot(command_prefix='.')
-token = ''
+TOKEN = ''
 
 @client.event
 async def on_ready():
     print("Online.")
 
-client.run(token)
+@client.command()
+async def opgg(ctx, *, username):
+    username = username.replace(' ', '%20')
+    await ctx.send(f'https://na.op.gg/summoner/userName={username}')
+
+client.run(TOKEN)
