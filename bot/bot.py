839ca9f6128bcecadcb564ee96a38df974385f12
edwardzhuang2001 <45517287+edwardzhuang2001@users.noreply.github.com>
2021-04-01 02:17:02 -0400

    Heroku deployment test

--- bot/bot.py
+++ bot/bot.py
@@ -1,8 +1,9 @@
 import discord
 from discord.ext import commands
+import os
 
 client = commands.Bot(command_prefix='.')
-TOKEN = ''
+TOKEN = os.getenv('DISCORD_TOKEN')
 
 @client.event
 async def on_ready():
