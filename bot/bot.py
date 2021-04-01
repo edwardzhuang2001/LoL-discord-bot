e736da082269430ec74ae0bd38de90e6429f27d1
edwardzhuang2001 <45517287+edwardzhuang2001@users.noreply.github.com>
2021-04-01 02:09:06 -0400

    Added required files for Heroku

--- bot/bot.py
+++ bot/bot.py
@@ -13,4 +13,16 @@ async def opgg(ctx, *, username):
     username = username.replace(' ', '%20')
     await ctx.send(f'https://na.op.gg/summoner/userName={username}')
 
+@client.command()
+async def profile(ctx, *, username):
+    await ctx.send()
+
+@client.command()
+async def recent(ctx, *, username):
+    await ctx.send()
+
+@client.command()
+async def live(ctx, *, username):
+    await ctx.send()
+
 client.run(TOKEN)
